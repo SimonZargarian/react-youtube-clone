0fb0c7aab6cf12641ab59ce5576d891d03f7e381
Simon Zargarian <simonzargariancv@gmail.com>
2024-05-25 12:08:09 +0200

    Initial commit

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,23 +1,19 @@
-import axios from 'axios';  // Import the axios library for making HTTP requests
+import axios from "axios";
 
-// Define a constant for the base URL of the YouTube v3 API hosted on RapidAPI
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
-// Define default options for all axios requests made using this instance
 const options = {
   params: {
-    maxResults: 50,  // Set the maximum number of results returned from the API to 50
+    maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,  // Use the API key stored in environment variables
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',  // Specify the host for the RapidAPI YouTube v3 API
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
 
-// Define and export an asynchronous function to fetch data from the API
 export const fetchFromAPI = async (url) => {
-  // Append the endpoint (url) to the BASE_URL and send a GET request with the specified options
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-  return data;  // Return the data from the response
+  return data;
 };
